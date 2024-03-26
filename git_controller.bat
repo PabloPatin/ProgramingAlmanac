6564ca0ca3db12622fc8bcb13632691d6c443bb8
PabloPatin <pashka.lop@mail.ru>
2024-03-27 00:35:52 +0300

    Library update

--- git_controller.bat
+++ git_controller.bat
@@ -1,31 +1,32 @@
 @echo off
 
+set GIT_EXE="C:\Program Files\Git\bin\git.exe"
 set LOG_FILE="D:\ProgramingAlmanac\log.txt"
 
 echo. > %LOG_FILE%
 
 :main
-set REPO_URL="https://github.com/PabloPatin/ProgramingAlmanarc.git"
+set REPO_URL="git@github.com:PabloPatin/ProgramingAlmanac.git"
 set BRANCH="main"
 set COMMIT_MESSAGE="Library update"
 
 echo [%DATE% %TIME%] - Запуск программы >> %LOG_FILE%
 
 call :log Конфигурация пользователя git
-git config user.email "pashka.lop@mail.ru" >> %LOG_FILE% 2>&1
-git config user.name "PabloPatin" >> %LOG_FILE% 2>&1
+%GIT_EXE% config user.email "pashka.lop@mail.ru" >> %LOG_FILE% 2>&1
+%GIT_EXE% config user.name "PabloPatin" >> %LOG_FILE% 2>&1
 
 call :log Переход в директорию вашего проекта
 cd /D "D:\ProgramingAlmanac"
 
 call :log Добавление всех изменений в индекс
-git add . >> %LOG_FILE% 2>&1
+%GIT_EXE% add . >> %LOG_FILE% 2>&1
 
 call :log Создание коммита с указанным сообщением
-git commit -m %COMMIT_MESSAGE% >> %LOG_FILE% 2>&1
+%GIT_EXE% commit -m %COMMIT_MESSAGE% >> %LOG_FILE% 2>&1
 
 call :log Отправка изменений на сервер
-git push origin %BRANCH% >> %LOG_FILE% 2>&1
+%GIT_EXE% push origin %BRANCH% >> %LOG_FILE% 2>&1
 exit
 
 :log
