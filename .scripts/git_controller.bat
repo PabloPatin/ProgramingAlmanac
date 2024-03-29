04a72e3bf3a823f21b99121478b9bb8a99653570
PabloPatin <pashka.lop@mail.ru>
2024-03-29 19:18:05 +0300

    Library update

--- .scripts/git_controller.bat
+++ .scripts/git_controller.bat
@@ -6,7 +6,6 @@ set LOG_FILE="D:\ProgramingAlmanac\.logs\log.txt"
 echo. > %LOG_FILE%
 
 :main
-%GIT_EXE% config core.sshCommand "ssh -i C:\Users\Днс\.ssh\id_rsa"
 %GIT_EXE% config --global --add safe.directory D:/ProgramingAlmanac
 
 set REPO_URL="git@github.com:PabloPatin/ProgramingAlmanac.git"
